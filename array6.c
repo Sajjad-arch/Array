d80c84ec0b73361708a3a7d4a609935737cdf5bf
Sajjad-arch <sajjadulislam870@gmail.com>
2025-07-26 23:55:08 +0600

    Update array6.c

--- array6.c
+++ array6.c
@@ -2,33 +2,52 @@
 int main(void)
 
 {
-    int i,n,ne;
+    int i,n,ne,m;
 
     printf("Enter the size of an array: ");
     scanf("%d",&n);
     int arr[n];
+   int numberofelements=0;
 
 
-    printf("Enter the %d elments: \n",n);
-    for(i=1; i<=n; i++)
+    printf("Enter number of elements in the array\n");
+    scanf("%d", &m);
+
+
+    printf("Enter the %d elments: \n",m);
+    for(i=0; i<m; i++)
     {
         scanf("%d",&arr[i]);
+        numberofelements++;
     }
 
 
 
-    printf("Enter the elment want to insert: ");
-    scanf("%d",&ne);
-    arr[n]=ne;
-
-
-
-    for(i=1; i<=n; i++)
+    if(numberofelements<n)
     {
-       printf("%d \n",arr[i]);
+        printf("Enter the elment want to insert: ");
+    scanf("%d",&ne);
+
+         arr[numberofelements]=ne;
+          numberofelements++;
+
+
+
+
+    for(i=0; i<numberofelements; i++)
+    {
+       printf("%d ",arr[i]);
 
     }
+
+
+
+}
+else {
+        printf("Insertion is not possible because the array is full!!!\n");
+    }
+
 
     return 0;
 
-}
+    }
